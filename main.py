2cdedad6de7205b39b845afb0ba0d8b34f2766bf
git-sandro <sandro-zimmermann@outlook.com>
2025-08-26 17:29:27 +0200

    My first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+import sys
+from google import genai
+from dotenv import load_dotenv
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    if len(sys.argv) == 1:
+        sys.exit(1)
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', 
+        contents=sys.argv[1]
+    )
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
